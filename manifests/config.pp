95f8a13df319ace57c4366155f088e1855383d06
Matthew Schmitt <matthew.schmitt@riskiq.net>
2016-04-19 12:52:08 -0700

    Adding template for config Updating config to create config file

--- manifests/config.pp
+++ manifests/config.pp
@@ -3,4 +3,11 @@
 # This class is called from uwsgi for service config.
 #
 class uwsgi::config {
+  file { '/etc/uwsgi.ini':
+    ensure  => present,
+    owner   => uwsgi,
+    group   => uwsgi,
+    mode    => '0644',
+    content => template('uwsgi/uwsgi.ini.erb'),
+  }
 }
